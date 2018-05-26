1615d07f893b90fa92ece8a3854ec12e18342762
Kevin Fries <kelfink@yahoo.com>
2018-05-26 10:40:38 -0700

    Split CRA app into main and header

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class Main extends Component {
+  render() {
+    return (
+      <p className="App-intro">
+        second save of <code>src/App.js</code> and save to reload.
+      </p>
+    )
+  }
+}
+
